a78dffc13b0d0fd9622e839d97a42d18a4ae6751
Yanic Dober <yanic.dobler@gmail.com>
2024-06-04 14:56:52 +0200

    Changed package name

--- getting-started/src/main/java/com/itdobler/oss/halma/GreetingResource.java
+++ getting-started/src/main/java/com/itdobler/oss/halma/GreetingResource.java
@@ -1,4 +1,4 @@
-package org.acme;
+package com.itdobler.oss.halma;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
--- getting-started/src/native-test/java/com/itdobler/oss/halma/GreetingResourceIT.java
+++ getting-started/src/native-test/java/com/itdobler/oss/halma/GreetingResourceIT.java
@@ -1,4 +1,4 @@
-package org.acme;
+package com.itdobler.oss.halma;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
