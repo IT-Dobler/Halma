a86aa1e69f63ca83f44da7219973e547593620e9
Yanic Dober <yanic.dobler@gmail.com>
2024-06-04 16:06:12 +0200

    Added a minimal postgres docker-compose and a environment template file

--- quarkus-backend/build.gradle.kts
+++ quarkus-backend/build.gradle.kts
@@ -13,6 +13,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-rest")
     implementation("io.quarkus:quarkus-arc")
