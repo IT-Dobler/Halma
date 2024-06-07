ad9a89634b1d88525d6d833797e01467d2841e9f
Yanic Dober <yanic.dobler@gmail.com>
2024-06-07 23:36:59 +0200

    First attempt at porting and understanding the quarkus starter.

--- ng-frontend/libs/generated-api-client/src/lib/api.provider.ts
+++ ng-frontend/libs/generated-api-client/src/lib/api.provider.ts
@@ -1,13 +1,3 @@
-/*
- * Copyright (C) 2023 DV Bern AG, Switzerland
- *
- * Das vorliegende Dokument, einschliesslich aller seiner Teile, ist urheberrechtlich
- * geschuezt. Jede Verwertung ist ohne Zustimmung der DV Bern AG unzulaessig. Dies gilt
- * insbesondere für Vervielfaeltigungen, die Einspeicherung und Verarbeitung in
- * elektronischer Form. Wird das Dokument einem Kunden im Rahmen der Projektarbeit zur
- * Ansicht uebergeben, ist jede weitere Verteilung durch den Kunden an Dritte untersagt.
- */
-
 import type {EnvironmentProviders} from '@angular/core';
 import {makeEnvironmentProviders} from '@angular/core';
 
